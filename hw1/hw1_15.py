46815db20925f7bcca72c4aec3495933cc72b3cf
JiahaoTang <jiahaotang1994@gmail.com>
2018-01-14 14:21:05 -0500

    PLA finished

--- hw1/hw1_15.py
+++ hw1/hw1_15.py
@@ -6,31 +6,42 @@ def sign(x):
 	else:
 		return -1
 
+def isFinished(w, X, Y):
+	for i in range(X.shape[0]):
+		res = w[0] * X[i][0] + w[1] * X[i][1] + w[2] * X[i][2] + w[3] * X[i][3] + w[4] * X[i][4]
+		if sign(res) != Y[i]:
+			return i
+	return X.shape[0]
+    
 def main():
+	# Read data from database
 	data = np.loadtxt('hw1_15_train.txt')
+	inputX = np.ndarray((400, 5))
+	inputY = np.ndarray((400, 1))
+	for i in range(400):
+		inputX[i][0] = 1
+		inputX[i][1] = data[i][0]
+		inputX[i][2] = data[i][1]
+		inputX[i][3] = data[i][2]
+		inputX[i][4] = data[i][3]
+		inputY[i]    = data[i][4]
+
 	# Initlized weight and continuous correct number.
-	w = np.array([0, 0, 0, 0], dtype='float')
-	correctNum = 0
-	isFinished = 0
+	w = np.array([0, 0, 0, 0, 0], dtype='float')
 	step = 0
-	i = 0
 	dataSize = data.shape[0]
 
-	while not isFinished:
-		res = w[0] * data[i][0] + w[1] * data[i][1] + w[2] * data[i][2] + w[3] * data[i][3]
-		if sign(res) == data[i][4]:
-			correctNum += 1
-			i += 1
-		else:
-			w += np.array([data[i][0], data[i][1], data[i][2], data[i][3]]) * data[i][4]
-			step += 1
-			correctNum = 0
-			print("step {}: index {} is wrong".format(step, i))
-			i = 0
+	while True:
+		returnValue = isFinished(w, inputX, inputY)
 
-		if correctNum == dataSize:
-			isFinished = 1
-		print(w)
+		if  returnValue == dataSize:
+			break
+		else:
+			i = returnValue
+			# w = w + x * y (update weight vector)
+			w += inputX[i] * inputY[i] 
+			step += 1
+			print("step {}: index {} is wrong".format(step, i))
 
 	print('The final weight is {}.'.format(w))
 	print('Total train times is {}.'.format(step))
---
+++ hw1/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+data = np.loadtxt('hw1_15_train.txt')
+inputX = np.ndarray((400, 5))
+inputY = np.ndarray((400))
+for i in range(400):
+	inputX[i][0] = 1
+	inputX[i][1] = data[i][0]
+	inputX[i][2] = data[i][1]
+	inputX[i][3] = data[i][2]
+	inputX[i][4] = data[i][3]
+	inputY[i] = data[i][4]
+print(inputY)
